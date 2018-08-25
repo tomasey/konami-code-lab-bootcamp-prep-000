3a820e6dccb1263129b97865b69beafed2e5df42
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-25 18:59:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+//
+
+document.body.addEventListener('keydown', init(e));
+
+//
+
+var index = 0;
+
+//
+
+function init(e) {
+var key = e.key;
+  if (key === codes[index]) {
+    index ++;
+  }
+  if (index === codes.length) {
+    alert("Hurray!");
+    
+    index = 0;
+  }
+  else {
+    index = 0;
+  }
 }
